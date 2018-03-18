a8d061a5e7431afe22b159efb4c82d382626288a
FuzzyJones <donald.jones83@gmail.com>
2018-03-17 22:59:43 -0400

    basic html created, server.js and htmlRoutes.js working

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
