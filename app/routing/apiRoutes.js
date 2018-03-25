83dbe11b5c54a3711ef55442bdb80eeb81db4f47
FuzzyJones <donald.jones83@gmail.com>
2018-03-24 21:02:18 -0400

    getting best match, need to fix modal

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,44 +9,48 @@ module.exports = function (app) {
 
     app.post("/api/friends", function(req, res) {
         // store the request data in a variable
-        var newfriend = req.body;
+        var user = req.body;
 
         // will eventually store the name and photo of the best match determined below in a variable to be returned in the res.json
-        var friendMatch = {
-            name: newfriend.name,
-            photo: newfriend.photo,
-        };
+        var friendMatch;
 
-        // need a difference variable to compare the scores of the new friend and existing friend
-        var difference = 0;
-
+        var maxDifference = 44;
+        
+        var bestMatchNameArray = [];
+        var bestMatchPhotoArray = [];
+        
         // loop through the existing friends array from the friendsData.js file and any new friend pushed into that array
         for (i = 0; i < friends.length; i++) {
             // log name of friends in array for testing
-            console.log(friends[i].name);
-
+            // console.log(friends[i].name);
+            var totalDifference = 0;
             // now loop through the scores of each in the array to compare to the new user scores to find difference
-            for (j = 0; j < friends[i].scores.length; j++) {
-                // now storing the difference of each score of the new friend to that of each in the array
-                difference = Math.abs(parseInt(newfriend.scores[j]) - parseInt(friends[i].scores[j]));
+            for (j = 0; j < user.scores.length; j++) {
+                // now storing the difference of each score of the new friend to that of each in the friends array
+                var difference = Math.abs(parseInt(user.scores[j]) - parseInt(friends[i].scores[j]));
                 // console logging the difference for testing
-                console.log(difference);
-
-                // need to add the difference numbers for each friend in array to find a total difference for each
-                // need to find the lowest difference number to get a match
-                // need to store the name and photo of match in friendMatch variable and put in res.json below
+                // adding the difference of each value and creating a sum of total difference
+                totalDifference += difference;
+                // console log for testing
+                // console.log(totalDifference); 
+            }
+            
+            if (totalDifference < maxDifference) {
+                maxDifference = totalDifference;
+                friendMatch = friends[i];
                 
                 
             }
         }
-        
+
+        console.log(friendMatch);
 
         // makes int a positive number
         // Math.abs();
 
-        friends.push(newfriend);
+        friends.push(user);
         // need to change to friendMatch once found
-        res.json(newfriend);
+        res.json(friendMatch);
     });
 
 };
